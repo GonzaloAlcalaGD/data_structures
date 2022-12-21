65c3dac3bcef2a9b10a5a1d903b7fc7b02b0c8dd
GonzaloAlcalaGD <galcala@griddynamics.com>
2022-12-21 17:32:28 -0600

    add insert function

--- array_implementation.py
+++ array_implementation.py
@@ -39,10 +39,44 @@ class MyArray():
         else:
             return str(f'Out of bound error, index : {idx} doesn\'t exist inside the array')
 
+    
+    def pop(self):
+        """
+        Deletes the last item in the array and returns it.
+        """
+        if self.length == 0:
+            return str('Array\'s length it\'s 0, no item\'s left to pop')
+        else:
+            last_item = self.data[self.length - 1]
+            del self.data[self.length - 1]
+            self.length -= 1
+            return last_item
+
+
+    def insert(self, idx, item):
+        """
+        Inserts element in the desired index.
+        """
+        if idx > self.length-1:
+            print(f'Index: {idx} out of bound. Inserting at the end of the array.')
+            self.push(item)
+        else:
+            print('else')
+            self.length += 1
+            for index in range(self.length-1, idx, -1):
+                self.data[index] = self.data[index-1]
+            self.data[idx] = item
+
+
+
 
 first_array = MyArray()
 
+first_array.push(1)
+first_array.push(2)
+first_array.push(3)
+first_array.push(4)
 
-print(first_array)
+
 
 
