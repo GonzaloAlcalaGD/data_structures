6bf0b6dd71093613166ea34bde2753ea69d45629
GonzaloAlcalaGD <galcala@griddynamics.com>
2022-12-22 16:37:39 -0600

    add node class and initialize attributes

---
+++ linked_list.py
@@ -0,0 +1,12 @@
+class Node:
+    """
+    Node implementation in Python
+    """
+
+
+    def __init__(self, data) -> None:
+        """
+        Initialize attributes of our Node.
+        """
+        self.data = data
+        self.next = None
