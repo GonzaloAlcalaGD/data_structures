7e5b89a4fdac159792584f243fc407caf31cc826
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-03 16:15:02 -0600

    add double linked list class and initialize his attr

--- double_linked_list.py
+++ double_linked_list.py
@@ -7,3 +7,10 @@ class Node():
         self.data = data
         self.next = None
         self.previous = None
+
+
+class DoubleLinkedList():
+
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
