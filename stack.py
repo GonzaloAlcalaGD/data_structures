3bb6afdae6d49b2c15fec1fbd2f0114818ecd36d
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-04 16:05:39 -0600

    add Stack class with attributes

--- stack.py
+++ stack.py
@@ -4,3 +4,11 @@ class Node():
         self.data = data
         self.next = None
         self.length = 0
+
+
+class Stack():
+
+    def __init__(self) -> None:
+        self.top = None
+        self.bottom = None
+        self.length = 0
