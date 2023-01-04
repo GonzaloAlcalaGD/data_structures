f61a45ab25c05018bf54662fde2092549c75724b
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-04 16:09:13 -0600

    add function structures for stack class

--- stack.py
+++ stack.py
@@ -11,3 +11,31 @@ class Stack():
         self.top = None
         self.bottom = None
         self.length = 0
+    
+
+    def peek(self) -> Node:
+        """
+        Returns the first node in the stack
+        """
+        pass
+    
+
+    def push(self, data) -> Node:
+        """
+        Adds a node into the top of the stack
+        """
+        pass
+
+
+    def pop(self) -> Node:
+        """
+        Removes and returns the top node in the stack
+        """
+        pass
+
+    
+    def is_empty(self) -> bool:
+        """
+        Returns true if the stack it's empty or false if is not.
+        """
+        pass
