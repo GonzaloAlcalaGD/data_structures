a04a1fee543a6031e427110283d0bb38a0496797
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-05 10:49:43 -0600

    add str representation of queue

--- my_queue.py
+++ my_queue.py
@@ -15,12 +15,28 @@ class Queue():
         self.length = 0
     
 
+    def __repr__(self):
+
+        node = self.first
+        
+
+        while node is not None:
+            print(node.data)
+            print('↑')
+            node = node.next
+        
+        return 'None'
+
+
     def peek(self) -> Node:
         """
         Returns the first node from our queue
         """
-        return
-
+        if  self.is_empty():
+            return 'Queue it\'s empty'
+        else:
+            return self.first.data
+            
     
     def enqueue(self, data) -> None:
         """
@@ -44,8 +60,18 @@ class Queue():
         """
         Dequeues the first node in our queue
         """
-        return
-    
+        holder = self.first.next
+
+        if holder is None:
+            self.first = None
+            self.length -= 1
+            return
+        else:
+            self.first.next = None
+            self.first = holder
+            self.length -= 1
+            return
+
 
     def is_empty(self) -> bool:
         """
@@ -55,3 +81,14 @@ class Queue():
             return True
         else:
             return False
+
+my_Queue = Queue()
+
+for i in range(5):
+    my_Queue.enqueue(i)
+
+print(f'First: {my_Queue.peek()}')
+print(my_Queue)
+my_Queue.dequeue()
+print(f'First: {my_Queue.peek()}')
+print(my_Queue)
