a6f4ef423a83d148644a83b0360954668e0b562c
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-05 09:48:32 -0600

    add queue class with initial attributes

--- my_queue.py
+++ my_queue.py
@@ -1,5 +1,15 @@
 class Node():
 
     def __init__(self, data) -> None:
+
         self.data = data
         self.next = None
+
+class Queue():
+
+
+    def __init__(self) -> None:
+        
+        self.first = None
+        self.last = None
+        self.length = 0
