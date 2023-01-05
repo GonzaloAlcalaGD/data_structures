86d1772d66568174a03ccf3166bd00ac3fe460bf
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-05 09:57:04 -0600

    add queue function structure

--- my_queue.py
+++ my_queue.py
@@ -13,3 +13,31 @@ class Queue():
         self.first = None
         self.last = None
         self.length = 0
+    
+
+    def peek(self) -> Node:
+        """
+        Returns the first node from our queue
+        """
+        return
+
+    
+    def enqueue(self, data) -> None:
+        """
+        Enqueues the node into the last position of our queue
+        """
+        return
+
+    
+    def dequeue(self) -> None:
+        """
+        Dequeues the first node in our queue
+        """
+        return
+    
+
+    def is_empty(self) -> bool:
+        """
+        Returns true if the queue it's empty otherwise false.
+        """
+        return
