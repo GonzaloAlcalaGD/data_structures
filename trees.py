14f502656485b01c461e6ee034279ce748817f4d
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-06 11:35:33 -0600

    add insert function

--- trees.py
+++ trees.py
@@ -12,3 +12,31 @@ class Tree():
     def __init__(self) -> None:
         
         self.root = None
+
+    
+    def insert(self, data) -> None:
+        """
+        Inserts the node in the root if the roots it's none
+        Otherwise traverses the tree comparing the left and right values.
+        """
+        node = Node(data)
+        current_node = self.root
+
+        if self.root is None:
+            self.root = node
+            return
+        
+        while True:
+            # Left path
+            if data < current_node.data and current_node.left is None: 
+                current_node.left = node
+                return
+            else:
+                current_node = current_node.left
+
+            # Right path
+            if data > current_node.data and current_node.right is None:
+                current_node.right = node
+                return
+            else:
+                current_node = current_node.right
