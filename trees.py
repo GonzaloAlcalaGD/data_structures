639efa417777bd8fce08166b7252dae8bd68446c
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-06 11:18:39 -0600

    add class tree and initialize attributes

--- trees.py
+++ trees.py
@@ -1,6 +1,14 @@
 class Node():
 
     def __init__(self, data) -> None:
+
         self.data = data
         self.left = None
         self.right = None
+
+
+class Tree():
+
+    def __init__(self) -> None:
+        
+        self.root = None
