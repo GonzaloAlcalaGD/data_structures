61c8beb6c23175159f574c58f97eafe8864a3cdf
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-06 17:10:44 -0600

    add remove function

--- trees.py
+++ trees.py
@@ -73,7 +73,7 @@ class Tree():
 
     def lookup(self, data) -> Node:
         """
-        Traverses the Tree looking for the node, if it exist returns True
+        Traverses the Tree looking for the node, if it exist returns the actual Node
         otherwhise returns False
         """
         current_node = self.root
@@ -88,8 +88,91 @@ class Tree():
             
             if data < current_node.data: # Left path
                 current_node = current_node.left
-            else:
+
+            if data > current_node.data:
                 current_node = current_node.right # Right path
+
+    
+    def remove(self, data) -> Node:
+        """
+        Traverses the Tree looking for the desired Node and removes it 
+        And reconfigures both child and parent nodes if needed.
+        """
+        current_node = self.lookup(data)
+        parent_node = None
+
+        while current_node:
+            
+            # Find the node and his parent node
+            if data < current_node.data: # Left path
+                parent_node = current_node
+                current_node = current_node.left
+
+            if data > current_node.data:
+                parent_node = current_node
+                current_node = current_node.right # Right path
+            
+            if data == current_node.data:
+
+                # Option 1 - No right child
+                if current_node.right is None:
+
+                    if parent_node is None:
+                        self.root = current_node.left
+                    
+                    #If the parent it's greather than our current node, make our current node left a child a child of parent.
+                    if parent_node.data > current_node.data:
+                        parent_node.left = current_node.left 
+                    
+                    # If the parents it's lesser than our current node, make our current node left a child a right child of parent
+                    if parent_node.data < current_node.data:
+                        parent_node.right = current_node.left
+                
+                # Option 2 - Right child without left child
+                elif current_node.right.left is None:
+
+                    current_node.right.left = current_node.left
+
+                    if parent_node.left is None:
+                        self.root = current_node.right
+                    
+                    # If parent is greather than our current node, make our current right a child of the left the parent
+                    if parent_node.data > current_node.data:
+                        parent_node.left = current_node.right
+                    
+                    # If parent is lesser than our currend node, make our current right a child of the right 
+                    if parent_node.data < current_node.data:
+                        parent_node.right = current_node.right
+                    
+                # Option 3 - Right child that has a left child
+                else:
+                    
+                    left_most = current_node.right.left
+                    left_most_parent = current_node.right
+
+                    while left_most is not None:
+                        left_most_parent = left_most
+                        left_most = left_most.left
+                    
+                    #Parent's left subtree is now leftmost's right subtree
+                    left_most_parent.left = left_most.left
+                    left_most.left = current_node.left
+                    left_most.right = current_node.right
+
+                    if parent_node is None:
+                        self.root = left_most
+                    
+                    if current_node.data < parent_node.data:
+                        parent_node.left = left_most
+                    
+                    if current_node.data > parent_node.data:
+                        parent_node.right = left_most
+
+            return True
+
+
+
+        
     
         
 
