a0dadbf69ef5dc99cb4319a3f08e802368943478
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-09 16:14:47 -0600

    add add_vertex function

--- graph.py
+++ graph.py
@@ -2,8 +2,20 @@ class Graph:
 
     def __init__(self) -> None:
         self.n_of_nodes = 0
-        self.adjacent_list = 0
+        self.adjacent_list = {}
     
 
     def __str__(self) -> str:
         return str(self.__dict__)
+    
+
+    def add_vertex(self, node) -> None:
+        """
+        Adds a vertex into our adjacent list
+        """
+        self.adjacent_list[node] = []
+        self.n_of_nodes += 1
+        return
+    
+
+    
