d8049abddc3d8fbbc4abadc80b345090dc5f28ba
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-09 16:10:39 -0600

    add graph class and initialize values and str representation

---
+++ graph.py
@@ -0,0 +1,9 @@
+class Graph:
+
+    def __init__(self) -> None:
+        self.n_of_nodes = 0
+        self.adjacent_list = 0
+    
+
+    def __str__(self) -> str:
+        return str(self.__dict__)
